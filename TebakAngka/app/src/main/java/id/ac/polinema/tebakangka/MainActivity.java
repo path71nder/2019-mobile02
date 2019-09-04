ff04d72ab29017f56192edea2ae727fb28846a97
path71nder <sevillebayuaji@gmail.com>
2019-09-04 17:21:47 +0700

    Tugas: Aplikasi Tebak Angka

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -13,23 +13,25 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
 	//  TODO: deklarasikan variabel di sini
+	private int angka;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		// TODO: bind layout di sini
-
+		angka = findViewById(R.id.number_input);
 	}
 
 	// TODO: generate angka random di sini
 	private void initRandomNumber() {
 		Random tebak = new Random();
-		int angka = tebak.nextInt();
+		angka = tebak.nextInt();
 	}
 
 	public void handleGuess(View view) {
 		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+		initRandomNumber();
 	}
 
 	public void handleReset(View view) {
